b9fa6435b2cf3da3ec838a8b6f282c6311c43da0
julianshen <julianshen@gmail.com>
2012-08-27 15:15:37 +0800

    add v8 snapshot

--- jni/Android.mk
+++ jni/Android.mk
@@ -5,16 +5,18 @@ include $(CLEAR_VARS)
 NODE_VERSION:= 0.8.8
 NODE_PATH := node-v$(NODE_VERSION)
 NODE_DEP_PATH := $(NODE_PATH)/deps
-$(info TARGET ARCH : $(TARGET_ARCH))
+PREBUILT_EXEC_PATH := $(LOCAL_PATH)/prebuilt/$(HOST_OS)/bin
+
+$(info HOST OS: $(HOST_OS) TARGET ARCH : $(TARGET_ARCH))
 include $(NODE_DEP_PATH)/openssl/openssl/Android.mk
 include $(NODE_DEP_PATH)/zlib/Android.mk
 include $(NODE_DEP_PATH)/http_parser/Android.mk
 include $(NODE_DEP_PATH)/uv/Android.mk
 
 #for V8
-ENABLE_V8_SNAPSHOT = false
+ENABLE_V8_SNAPSHOT = true
 include $(NODE_DEP_PATH)/v8/Android.libv8.mk
-include $(NODE_DEP_PATH)/v8/Android.v8shell.mk
+#include $(NODE_DEP_PATH)/v8/Android.v8shell.mk
 
 #node.js
 include $(NODE_PATH)/Android.mk
