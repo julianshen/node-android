fa874e290a4589475413dd2086d3d0319d1b4da4
Julian Shen <julianshen@jlnsmbp.(none)>
2012-09-26 22:49:13 +0800

    add tag to demo script

--- assets/nodejs/app.js
+++ assets/nodejs/app.js
@@ -33,6 +33,6 @@ app.get('/users', user.list);
 http.createServer(app).listen(app.get('port'), function(){
     console.log("Express server listening on port " + app.get('port'));
     if(alog) {
-        alog.d("Express server listening on port " + app.get('port'));
+        alog.d("nodejs-demo", "Express server listening on port " + app.get('port'));
     }
 });
