d6c319465d76ce92f60fdf3ca09de1b95ea4300f
Julian Shen <julianshen@gmail.com>
2014-05-26 13:44:55 +0800

    Includ all symbol in libnode.a

--- jni/Android.mk
+++ jni/Android.mk
@@ -51,7 +51,8 @@ include $(CLEAR_VARS)
 LOCAL_MODULE    := nodeJNI
 
 LOCAL_C_INCLUDES := $(NODE_INC_PATH) $(NODE_PATH)/deps/v8/include jni-bridge
-LOCAL_STATIC_LIBRARIES := node uv cares chrome_zlib v8_base.arm http_parser v8_nosnapshot.arm openssl
+LOCAL_WHOLE_STATIC_LIBRARIES := node
+LOCAL_STATIC_LIBRARIES := uv cares chrome_zlib v8_base.arm http_parser v8_nosnapshot.arm openssl
 LOCAL_LDLIBS := -llog -landroid 
 
 LOCAL_SRC_FILES := jni-bridge/node_jni.cc \
